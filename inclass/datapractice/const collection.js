61319cc3db3add57a9442b967dc3675d959db647
bradleeeey11 <114003819+bradleeeey11@users.noreply.github.com>
2023-03-08 22:30:32 -0500

    3/8/23

---
+++ inclass/datapractice/const collection.js
@@ -0,0 +1,91 @@
+const collection = [
+{
+    'title': 'Youre Too Close'
+    'group': 'Bumper Sticker'
+    'color': 'pink'
+    'year': '1970'
+    }
+{
+'title': 'Dont Tread On Me'
+'group': 'Bumper Sticker'
+'color': 'green'
+'year': '1970'
+}
+{
+    'title': 'Wake Me Up'
+    'group': 'Bumper Sticker'
+    'color': 'orange'
+    'year': '1970'
+    }
+
+{
+'title': 'Keep On Bikin'
+'group': 'Bumper Sticker'
+'color': 'yellow'
+'year': '1970'
+}
+
+{
+'title': 'Baseball Mickey'
+'group': 'Disney'
+'color': 'var'
+'year': '1975'
+}
+
+{
+'title': 'Golf Goofy'
+'group': 'Disney'
+'color': 'var'
+'year': '1975'
+}
+
+{
+'title': 'If the Shoe Fits, Wear It'
+'group': 'Disney'
+'color': 'var'
+'year': '1975'
+}
+
+{
+'title': 'Rich Man'
+'group': 'Disney'
+'color': 'var'
+'year': '1975'
+}
+
+{
+'title': 'Smomew'
+'group': 'Disney'
+'color': 'var'
+'year': '1975'
+}
+
+{
+'title': 'Youre Breakin my Heart'
+'group': 'Disney'
+'color': 'var'
+'year': '1975'
+}
+
+{
+'title': 'HO HI HO State'
+'group': 'Disney'
+'color': 'var'
+'year': '1975'
+}
+
+{
+'title': 'MI TEA'
+'group': 'Disney'
+'color': 'var'
+'year': '1975'
+}
+
+{
+'title': 'PURRDUE'
+'group': 'Disney'
+'color': 'var'
+'year': '1975'
+}
+
+]
