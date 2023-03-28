a7dfa8021e69e303ec7df89d7043c5fa24248b67
bradleeeey11 <114003819+bradleeeey11@users.noreply.github.com>
2023-03-28 10:33:48 -0400

    3/28/23

--- project2/assets/collection.js
+++ project2/assets/collection.js
@@ -8,14 +8,15 @@ const renderItems = (collection) => {
 	collection.forEach(item => {
 		const listItem = document.createElement('li') // Make the `li`
 
-		// You can make each element inside of that…
-		const itemTitle = document.createElement('h2') // Make an `h2`
-		itemTitle.innerHTML = item.title // Put the JSON title inside
-		listItem.appendChild(itemTitle) // And add it to the `li`!
 
 		const itemImage = document.createElement('img') // And an image
 		itemImage.src = item.image // Set the `src` attribute from the JSON
 		listItem.appendChild(itemImage) // And add that too
+
+				// You can make each element inside of that…
+				const itemTitle = document.createElement('h2') // Make an `h2`
+				itemTitle.innerHTML = item.title // Put the JSON title inside
+				listItem.appendChild(itemTitle) // And add it to the `li`!
 
 
 		// This can get annoying, so we can use “template literals” instead
